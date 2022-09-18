2cad546c56e96b49def09e67e9649fc01ebeb348
Deneb Hernandez <denebhdz@gmail.com>
2022-09-18 17:35:01 -0600

    initial commit

---
+++ app.py
@@ -0,0 +1,28 @@
+import datetime
+import pandas as pd
+from pathlib import Path
+from docxtpl import DocxTemplate
+
+today = datetime.datetime.today().strftime("%A, %w de %B")
+month_today = datetime.datetime.today().strftime("%B")
+
+base_dir = Path(__file__).parent
+template_path = base_dir / "template.docx"
+excel_path = base_dir / "datos.xlsx"
+output_dir = base_dir / f"cartas-{today}"
+
+output_dir.mkdir(exist_ok=True)
+
+df = pd.read_excel(excel_path, sheet_name="Sheet1")
+
+for record in df.to_dict(orient="records"):
+    record['fecha_actual'] = today
+    record['mes_actual'] = month_today
+    record['monto_deuda'] = '{:,.2f}'.format(record['monto_deuda'])
+    # print(record)
+    doc = DocxTemplate(template_path)
+    doc.render(record)
+    output_path = output_dir / f"{record['nombre_estudiante']}-carta.docx"
+    doc.save(output_path)
+
+
