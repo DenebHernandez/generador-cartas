5a5187577aef9e945216f2a8b663cd22b1c4f5b5
DenebHernandez <denebhdz@gmail.com>
2022-09-20 08:29:46 -0600

    Formated date data in spanish

--- app.py
+++ app.py
@@ -1,21 +1,26 @@
 import datetime
 import pandas as pd
+import win32com.client as win32
 from pathlib import Path
 from docxtpl import DocxTemplate
-import win32com.client as win32
+from babel.dates import format_datetime
 
-today = datetime.datetime.today().strftime("%A, %w de %B")
-month_today = datetime.datetime.today().strftime("%B")
+
+today = datetime.datetime.today()
+today_date = format_datetime(today, "d 'de' MMMM, y", locale="es")
+month_today = format_datetime(today, "MMMM", locale="es")
 
 base_dir = Path(__file__).parent
 template_path = base_dir / "template.docx"
 excel_path = base_dir / "datos.xlsx"
-output_dir = base_dir / f"cartas-{today}"
+output_dir = base_dir / f"cartas-{today_date}"
 
 output_dir.mkdir(exist_ok=True)
 
 df = pd.read_excel(excel_path, sheet_name="Sheet1")
 
+fecha_pago = df["fecha_pago"][0]
+fecha_pago = format_datetime(today, "d 'de' MMMM y", locale="es")
 
 def convert_to_pdf(doc):
     word = win32.DispatchEx('Word.Application')
@@ -27,9 +32,10 @@ def convert_to_pdf(doc):
 
 
 for record in df.to_dict(orient="records"):
-    record['fecha_actual'] = today
+    record['fecha_actual'] = today_date
     record['mes_actual'] = month_today
     record['monto_deuda'] = '{:,.2f}'.format(record['monto_deuda'])
+    record['fecha_pago'] = fecha_pago
     # print(record)
     doc = DocxTemplate(template_path)
     doc.render(record)
